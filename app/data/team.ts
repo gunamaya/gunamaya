91c3287e514f3ecd89a3e296d91672e5d55284c5
Anggi Susanto <83394718+anggi-susanto@users.noreply.github.com>
2026-05-21 21:51:56 +0700

    Update team.ts

--- app/data/team.ts
+++ app/data/team.ts
@@ -13,27 +13,15 @@ export const team: TeamMember[] = [
     country: "Indonesia",
   },
   {
-    name: "Yanuar Aditia",
-    role: "Senior Engineer",
-    user: "yanuaraditia",
-    country: "Indonesia",
-  },
-  {
-    name: "Daewu Bintara",
-    role: "Senior Engineer",
-    user: "daewu14",
-    country: "Indonesia",
-  },
-  {
     name: "M Adi F",
     role: "Senior Engineer",
     user: "muhadif",
     country: "Indonesia",
   },
   {
-    name: "Sofyan Saputra",
-    role: "Solution Architect",
-    user: "sofyan48",
+    name: "Jordi",
+    role: "AI Assistant",
+    user: "gnmy-jordi",
     country: "Indonesia",
-  },
+  }
 ];
